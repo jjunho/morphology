284d7ed941862102dae29197b87de08c1e24dd99
Juliano Paiva Junho <jjunho@gmail.com>
2025-01-26 01:53:03 +0900

    add CORS middleware and update dependencies for morphology API

--- app/Main.hs
+++ app/Main.hs
@@ -3,18 +3,35 @@
 
 module Main where
 
-import           Control.Monad.IO.Class (liftIO)
-import           Data.Aeson             (FromJSON, ToJSON, decode, encode)
-import           Data.Text              (Text, pack)
-import qualified System.Log.FastLogger  as FL
+import           Control.Monad.IO.Class      (liftIO)
+import           Data.Aeson                  (FromJSON, ToJSON, decode, encode)
+import           Data.Text                   (Text, pack)
+import qualified System.Log.FastLogger       as FL
 import           Web.Scotty
 
-import           NLP.Morphology.PT.Core (Citation)
-import           NLP.Morphology.PT.Verb (Paradigm, Txt (txt), VerbStructure,
-                                         mkParadigm)
+import           NLP.Morphology.PT.Core      (Citation)
+import           NLP.Morphology.PT.Verb      (Paradigm, Txt (txt),
+                                              VerbStructure, mkParadigm)
 
 import           ApiResponse
 import           Handlers
+import           Network.Wai                 (Middleware)
+import           Network.Wai.Middleware.Cors
+import           Network.Wai.Middleware.Cors (CorsResourcePolicy (..), cors,
+                                              simpleCorsResourcePolicy)
+
+-- Middleware para CORS
+corsMiddleware :: Middleware
+corsMiddleware = cors (const $ Just corsPolicy)
+
+-- Configuração do CORS
+corsPolicy :: CorsResourcePolicy
+corsPolicy =
+  simpleCorsResourcePolicy
+    { corsOrigins = Just (["http://localhost:1234"], True) -- Permitindo localhost:1234
+    , corsMethods = ["GET", "POST", "OPTIONS"] -- Métodos permitidos
+    , corsRequestHeaders = ["Content-Type"] -- Cabeçalhos permitidos
+    }
 
 -- Initialize Logger
 initLogger :: IO FL.LoggerSet
@@ -32,7 +49,9 @@ main :: IO ()
 main = do
   logger <- initLogger
   putStrLn "Starting Morphology API on http://0.0.0.0:3000..."
-  scotty 3000 (defineRoutes logger)
+  scotty 3000 $ do
+    middleware corsMiddleware
+    defineRoutes logger
   -- API Request Examples
   -- curl -X POST http://localhost:3000/paradigm \
   --   -H "Content-Type: application/json" \
